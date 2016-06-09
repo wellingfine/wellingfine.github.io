f96d3b3e4c87734169d26d7858c203ad884a914d
welling <wellingfine@qq.com>
2016-06-09 22:12:30 +0800

    test seajs's load

--- lab/seajs/module/main.js
+++ lab/seajs/module/main.js
@@ -1,12 +1,23 @@
 
 define(function(require, exports, module) {
 
-	
+
 	console.log('defining main')
 
-	// error can not define a dynamic module
-	// var echo=require('./ech'+'o')
-	var echo=require('./echo')
+	
+	
 
-	echo.log('call in main')
+	
+	function getEcho(){
+		// error can not define a dynamic module
+		// var echo=require('./ech'+'o')
+
+		var echo=require('./echo')
+
+
+		echo.log('call in main')
+	}
+
+	// when comment ,the module still loaded
+	// setTimeout(getEcho,30000)
 });
